d0b5da57832e8494e521501e9333a3246bb3276d
Mikhail <mschee@physics.utoronto.ca>
2019-05-17 09:29:21 -0400

    Added staircase background profile and matched initial conditions

--- current_code.py
+++ current_code.py
@@ -1,5 +1,7 @@
 """
-Dedalus script for 2D Rayleigh-Benard convection.
+Dedalus script for simulating internal waves through a vertically stratified
+fluid with a density profile similar to a double-diffusive staircase.
+Originally for 2D Rayleigh-Benard convection.
 
 Modified by Mikhail Schee, May 2019
 
@@ -48,6 +50,7 @@ from docopt import docopt
 # Parameters
 aspect_ratio = 4.0
 Lx, Lz = (aspect_ratio, 1.)
+z_b, z_t = (-Lz/2, Lz/2)
 # Placeholders for the 3 dimensionless numbers
 A1 = 1.1
 B2 = 2.2
@@ -65,56 +68,117 @@ if __name__ == '__main__':
 
 ###############################################################################
 
-'''
-# Defining the background density profile
-rho_min = 1.1
-rho_max = 1.2
-def density_profile(*args):
-	# this function applies its arguments and returns the profile
-	z = args[0].value
-	rho_min = args[1].value
-	rho_max = args[2].value
-	return rho_bar(z, rho_min, rho_max)
-
-def rho_bar(z, rho_min, rho_max):
-	# defines the background density to be a linear gradient
-	return z*(rho_max-rho_min)/Lz + (rho_max + rho_min)/2.0
-
-def profiling(*args, domain=domain, F=density_profile):
-	return de.operators.GeneralFunction(domain, layout='g', func=F, args=args)
-
-# Make the density profile parseable
-de.operators.parseables['DP'] = profiling
-'''
-
-'''
-# Adding a non-constant coefficient (ncc)
-ncc = domain.new_field(name='c')
-ncc['g'] = z**2
-ncc.meta['x', 'y']['constant'] = True
-problem.paramters['c'] = ncc
-'''
-###############################################################################
-
 # Create bases and domain
 x_basis = de.Fourier('x', 256, interval=(-Lx/2, Lx/2), dealias=3/2)
-z_basis = de.Chebyshev('z', 64, interval=(-Lz/2, Lz/2), dealias=3/2)
+z_basis = de.Chebyshev('z', 64, interval=(z_b, z_t), dealias=3/2)
 domain = de.Domain([x_basis, z_basis], grid_dtype=np.float64)
+# Initial conditions
+x = domain.grid(0)
+z = domain.grid(1)
+
+###############################################################################
+
+# Creating a wave-making boudary condition
+def wave_maker(x, op=0, L_x=2*np.pi, n=10):
+    if (op == 0):
+        # a plane wave
+        return 1.0
+    elif (op ==1):
+        # a simple, smooth bump function
+        return (((1-np.cos(2*np.pi/L_x * x))/2)**n)
+
+def BoundaryForcing(*args):
+    # this function applies its arguments and returns the forcing
+    t = args[0].value # this is a scalar; we use .value to get its value
+    x = args[1].data # this is an array; we use .data to get its values
+    ampl = args[2].value
+    freq = args[3].value
+    return ampl*wave_maker(x, 0, Lx)*np.cos(t*freq)
+
+def Forcing(*args, domain=domain, F=BoundaryForcing):
+    """This function takes arguments *args, a function F, and a domain and
+    returns a Dedalus GeneralFunction that can be applied."""
+    return de.operators.GeneralFunction(domain, layout='g', func=F, args=args)
+
+# now we make it parseable, so the symbol BF can be used in equations
+# and the parser will know what it is.
+de.operators.parseables['BF'] = Forcing
+
+###############################################################################
+
+# Parameters to determine a specific staircase profile
+n_layers = 2
+layer_ratio = 1
+val_bot = 1.0
+val_top = -1.0
+
+# Functions to define an arbitrary staircase profile
+def lin_profile(z, z_b, z_t, val_b, val_t):
+    # Creates a linear profile from (z_b, val_b) to (z_t, val_t)
+    values = 0*z
+    slope = (val_t - val_b) / (z_t - z_b)
+    values = slope*(z - z_b) + val_b
+    return values
+def val_ni(n, index, val_b, val_t):
+    i = index/2
+    # returns value at interface i for n layers (bottom interface=0, bottom layer=1)
+    return val_b + (i/n) * (val_t - val_b)
+def staircase(z, n, ratio, z_b, z_t, val_b, val_t):
+    # initialize array of values to be returned
+    values = 0*z
+    # find the thickness of the layers and the interfaces
+    th_l = (z_t - z_b) / (n + (n-1)/ratio)
+    th_i = th_l/ratio
+    # z is an array of height values
+    # function returns a corresponding array of values (density, for example)
+    z_i = z_b
+    index = 0 # even is interface, odd is layer
+    # Loop from bottom z to top z, alternating layers and interfaces
+    while (z_i < z_t):
+        # Layer
+        if (index%2 == 0):
+            index += 1
+            z_i += th_l
+            val_below = val_ni(n, index-1, val_b, val_t)
+            val_above = val_ni(n, index+1, val_b, val_t)
+            values[(z>(z_i-th_l))&(z<z_i)] = lin_profile(z[(z>(z_i-th_l))&(z<z_i)], z_i-th_l, z_i, val_below, val_above)
+        # Interface
+        else:
+            index += 1
+            z_i += th_i
+            values[(z>(z_i-th_i))&(z<z_i)] = val_ni(n, index, val_b, val_t)
+    return values
+
+###############################################################################
 
 # 2D Boussinesq hydrodynamics
 problem = de.IVP(domain, variables=['p','b','u','w','bz'])
 #problem = de.IVP(domain, variables=['p','b','u','w','bz','uz','wz'])
 problem.meta['p','b','u','w']['z']['dirichlet'] = True
-problem.parameters['A'] = A1 #(Rayleigh * Prandtl)**(-1/2)
-problem.parameters['B'] = B2 #(Rayleigh / Prandtl)**(-1/2)
-problem.parameters['C'] = C3 #F = 1
-'''
-problem.parameters['rho_min'] = rho_min
-problem.parameters['rho_max'] = rho_max
-'''
+# Parameters for the dimensionless numbers
+problem.parameters['A'] = A1
+problem.parameters['B'] = B2
+problem.parameters['C'] = C3
+# Parameters for boundary forcing
+problem.parameters['csq'] = 1. #c**2
+problem.parameters['ampl'] = 0.01
+problem.parameters['freq'] = 1.
+
+###############################################################################
+
+# Background Profile (bgpf) as an NCC
+bgpf = domain.new_field()
+bgpf.meta['x']['constant'] = True  # means the NCC is constant along x
+# Store profile in an array so it can be used for initial conditions later
+bgpf_array = staircase(z, n_layers, layer_ratio, z_b, z_t, val_bot, val_top)
+bgpf['g'] = bgpf_array
+problem.parameters['bgpf'] = bgpf  # pass function in as a parameter
+del bgpf
+
 ###############################################################################
 
 # Non-Dimensionalized Equations, no viscosity
+#problem.substitutions['v*del(u)']
 #   Mass conservation equation
 problem.add_equation("dx(u) + dz(w) = 0") #wz = 0")
 #   Energy equation (in terms of buoyancy)
@@ -122,7 +186,8 @@ problem.add_equation("dt(b) - B*(dx(dx(b)) + dz(bz)) = -(u*dx(b) + w*bz)")
 #   Horizontal momentum equation
 problem.add_equation("dt(u) + A*dx(p) = -(u*dx(u) + w*dz(u))")
 #   Vertical momentum equation
-problem.add_equation("dt(w) + A*dz(p) - b = -(u*dx(w) + w*dz(w))")#wz)")
+problem.add_equation("dt(w) + A*dz(p) - b = bgpf/(C**2) -(u*dx(w) + w*dz(w))")#wz)")
+#problem.add_equation("dt(w) + A*dz(p) - b = -(u*dx(w) + w*dz(w))")#wz)")
 
 # Required for differential equation solving in Chebyshev
 problem.add_equation("bz - dz(b) = 0")
@@ -141,13 +206,16 @@ problem.add_equation("bz - dz(b) = 0")
 #problem.add_bc("left(uz) = 0")
 #problem.add_bc("right(uz) = 0")
 # No-slip top/bottom boundaries?
-problem.add_bc("left(w) = 0")
-problem.add_bc("right(w) = 0", condition="(nx != 0)") # redunant in constant mode (nx==0)
+problem.add_bc("left(w) = 0", condition="(nx != 0)") # redunant in constant mode (nx==0)
+#problem.add_bc("right(w) = right(BF(t,x,ampl,freq))")
+problem.add_bc("right(w) = 0")
+#problem.add_bc("left(w) = 0")
 # Buoyancy = zero at top/bottom
 problem.add_bc("left(b) = 0")
+#problem.add_bc("right(b) = right(BF(t,x,ampl,freq))")
 problem.add_bc("right(b) = 0")
 # Sets gauge pressure to zero in the constant mode
-problem.add_bc("right(p) = 0", condition="(nx == 0)") # required because of above redundancy
+problem.add_bc("left(p) = 0", condition="(nx == 0)") # required because of above redundancy
 
 ###############################################################################
 
@@ -156,27 +224,21 @@ solver = problem.build_solver(de.timesteppers.RK222)
 logger.info('Solver built')
 
 # Initial conditions
-x = domain.grid(0)
-z = domain.grid(1)
 b = solver.state['b']
 bz = solver.state['bz']
-
+'''
 # Random perturbations, initialized globally for same results in parallel
 gshape = domain.dist.grid_layout.global_shape(scales=1)
 slices = domain.dist.grid_layout.slices(scales=1)
 rand = np.random.RandomState(seed=42)
 noise = rand.standard_normal(gshape)[slices]
-'''
-# Defining the background density gradient
-def bg_density(z, rho_min, rho_max):
-	# defines the background density to be a linear gradient
-	return z*(rho_max-rho_min)/Lz + (rho_max + rho_min)/2.0
-'''
+
 # Linear background + perturbations damped at walls
 zb, zt = z_basis.interval
 pert =  1e-3 * noise * (zt - z) * (z - zb)
-b['g'] = C3 * pert
-#b['g'] = (-1.0/(C3**2)) * bg_density(z, rho_min, rho_max)
+'''
+# Buoyancy initial conditions
+b['g'] = bgpf_array #C3 * pert
 b.differentiate('z', out=bz)
 
 ###############################################################################
