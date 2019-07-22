c025c64629392a11d588abfcf87ada99be321de5
Mikhail <mschee@physics.utoronto.ca>
2019-07-22 16:39:27 -0400

    Now points to Vanilla Dedalus directory

--- submit_job.sh
+++ submit_job.sh
@@ -24,19 +24,20 @@ DATE=`date +"%m-%d_%Hh%M"`
 # create a 2 digit version of CORES
 printf -v CO "%02d" $CORES
 JOBNAME="$DATE-2D_BF-n$CO"
-DIRECTORY='2D_boundary_forced_waves_v2'
+DIRECTORY='Dedalus/Vanilla_Dedalus'
+SUBDIRECT='2D_boundary_forced_waves_v2'
 
 set -x # echos each command as it is executed
 
 # Go into directory of job to run
-cd ${HOME}/Dedalus/Dedalus_Files/$DIRECTORY
+cd ${HOME}/$DIRECTORY/$SUBDIRECT
 # Pull from github the latest version of that project
 git pull
 # Copy that into the scratch directory, ignoring the .git/ directory and others
-cp -r ${HOME}/Dedalu/Dedalus_Files/$DIRECTORY ${SCRATCH}/Dedalus/Dedalus_Files/$DIRECTORY
+cp -r ${HOME}/$DIRECTORY/$SUBDIRECT ${SCRATCH}/$DIRECTORY/$SUBDIRECT
 #rsync -av --progress ${HOME}/Dedalus/Dedalus_Files/$DIRECTORY ${SCRATCH}/Dedalus/Dedalus_Files/ --exclude .git/ --exclude mp4s/ --exclude _boundary_forcing/ --exclude _code_checkpnts/ --exclude _energy_flux/ --exclude _misc/ --exclude snapshots/ --exclude ef_snapshots/ --exclude frames/ --exclude gifs/*
-mv ${SCRATCH}/Dedalus/Dedalus_Files/$DIRECTORY ${SCRATCH}/Dedalus/Dedalus_Files/$JOBNAME
-cd ${SCRATCH}/Dedalus/Dedalus_Files/$JOBNAME
+mv ${SCRATCH}/$DIRECTORY/$SUBDIRECT ${SCRATCH}/$DIRECTORY/$JOBNAME
+cd ${SCRATCH}/$DIRECTORY/$JOBNAME
 ls
 
 # Submit the job
