81e6f1567d7059066bc41b2ebca2725a9e9d1563
Mikhail <mschee@physics.utoronto.ca>
2019-07-23 10:22:47 -0400

    Trying to fix directory path

--- push_from_Niagara.sh
+++ push_from_Niagara.sh
@@ -3,8 +3,7 @@
 # Warning: this will overwrite the snapshot folders in
 #	the local directory without checking
 
-DIRECTORY='/scratch/n/ngrisoua/mschee/Dedalus/Vanilla_Dedalus'
 LOCAL_DIR=~/Documents/Dedalus_Projects/2D_boundary_forced_waves_v2/
 
-scp -r snapshots/ mschee@ngws19.atmosp.physics.utoronto.ca:$LOCAL_DIR
-scp -r ef_snapshots/ mschee@ngws19.atmosp.physics.utoronto.ca:$LOCAL_DIR
+scp -r snapshots/ mschee@ngws19.atmosp.physics.utoronto.ca:~/Documents/Dedalus_Projects/2D_boundary_forced_waves_v2/
+scp -r ef_snapshots/ mschee@ngws19.atmosp.physics.utoronto.ca:~/Documents/Dedalus_Projects/2D_boundary_forced_waves_v2/
