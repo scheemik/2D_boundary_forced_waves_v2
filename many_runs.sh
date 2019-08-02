bce05ac7d42be6099083a36e4cc1cb08a7b5dac7
Mikhail <mschee@physics.utoronto.ca>
2019-08-02 17:38:11 -0400

    Added script to run many runs in a row

---
+++ many_runs.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# A bash script to run the Dedalus python code many times
+# run.sh takes in these arguments:
+#	$ sh run.sh -n <name of particular run>
+#				-c <cores>
+#				-e <EF(1) or reproducing run(0)>
+#				-i <number of interfaces in background profile>
+#				-l <local(1) or Niagara(0)>
+#				-v <version: what scripts to run>
+#				-k <keep(1) or allow overwriting(0)>
+#				-t <test parameter>
+
+# if:
+# VER = 0 (Full)
+#	-> run the script, merge, plot EF if relevant, plot frames, create gif, create mp4
+# VER = 1
+#	-> run the script, merge, and plot EF if relevant
+# VER = 2
+#	-> run the script
+# VER = 3
+# 	-> merge, plot EF if relevant. plot frames, and create a gif
+# VER = 4
+#	-> create mp4 from frames
+
+echo 'Test parameter currently set to oscillation periods for BF ramp'
+sh run.sh -v 0 -i 0 -k 1 -t 0.5
+sh run.sh -v 0 -i 0 -k 1 -t 1.0
+sh run.sh -v 0 -i 0 -k 1 -t 1.5
+sh run.sh -v 0 -i 0 -k 1 -t 2.0
+sh run.sh -v 0 -i 0 -k 1 -t 2.5
+
+echo "Done"
