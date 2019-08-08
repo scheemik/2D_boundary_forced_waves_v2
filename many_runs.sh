d61973bb5d4270a01104a0ccd4a2e860c571443b
Mikhail <mschee@physics.utoronto.ca>
2019-08-08 11:50:39 -0400

    Preparing to run on Niagara

--- many_runs.sh
+++ many_runs.sh
@@ -22,9 +22,9 @@
 # VER = 4
 #	-> create mp4 from frames
 
-echo 'Test parameter currently set to forcing window slope'
-sh run.sh -v 0 -e 0 -i 2 -k 1 -t 20.0
-sh run.sh -v 0 -e 0 -i 1 -k 1 -t 20.0
+echo 'Test parameter currently set to forcing amplitude, A'
+sh run.sh -v 0 -e 0 -i 2 -k 1 -t 3.0e-4
+sh run.sh -v 0 -e 0 -i 1 -k 1 -t 3.0e-4
 #sh run.sh -v 0 -e 0 -i 1 -k 1 -t 30.0
 #sh run.sh -v 0 -e 0 -i 1 -k 1 -t 35.0
 
