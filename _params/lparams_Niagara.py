763bf3a97afa789624d3b0689f7d7be16e2d4a55
Mikhail <mschee@physics.utoronto.ca>
2019-08-08 11:52:12 -0400

    Ready for Niagara run

---
+++ _params/lparams_Niagara.py
@@ -0,0 +1,12 @@
+# Parameters for running on my local machine
+
+# Stop times for the simulation
+sim_period_stop = 20 # oscillation periods, time in seconds calculated below
+wall_time_stop = 60 # min
+
+# Determine whether adaptive time stepping is on or off
+adapt_dt = True
+
+# Number of points in each dimension
+n_x = 256
+n_z = 512
