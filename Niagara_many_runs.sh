dee3f4181f2344d391a259d13d3663d1d64ec73c
Mikhail <mschee@physics.utoronto.ca>
2019-08-09 13:17:53 -0400

    Different Niagara job

--- Niagara_many_runs.sh
+++ Niagara_many_runs.sh
@@ -38,8 +38,8 @@ fi
 #	-> create mp4 from frames
 
 echo 'Test parameter currently set to forcing amplitude, A'
-sh run.sh -c $CORES -v 1 -l 0 -e 0 -i 2 -k 1 -t 2.5e-4
-#sh run.sh -c $CORES -v 1 -l 0 -e 0 -i 1 -k 1 -t 2.5e-4
+#sh run.sh -c $CORES -v 1 -l 0 -e 0 -i 2 -k 1 -t 2.5e-4
+sh run.sh -c $CORES -v 1 -l 0 -e 0 -i 1 -k 1 -t 2.5e-4
 #sh run.sh -v 0 -e 0 -i 1 -k 1 -t 30.0
 #sh run.sh -v 0 -e 0 -i 1 -k 1 -t 35.0
 
