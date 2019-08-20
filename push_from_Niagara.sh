f3954e4985c691a22b243dd0c8ba67a4b52f687e
Mikhail <mschee@physics.utoronto.ca>
2019-08-20 11:49:38 -0400

    Changed file paths to experiments folder

--- push_from_Niagara.sh
+++ push_from_Niagara.sh
@@ -1,9 +1,26 @@
 #!/bin/bash
-# A bash script to push the snapshot folders from Niagara
-# Warning: this will overwrite the snapshot folders in
-#	the local directory without checking
+# A bash script to copy an experiment from Niagara to local
+# push_from_Niagara.sh takes in these arguments:
+#	$ sh push_from_Niagara.sh -n <name of particular run>
 
-LOCAL_DIR=~/Documents/Dedalus_Projects/2D_boundary_forced_waves_v2/
+while getopts n: option
+do
+	case "${option}"
+		in
+		n) NAME=${OPTARG};;
+	esac
+done
 
-scp -r snapshots/ mschee@ngws19.atmosp.physics.utoronto.ca:~/Documents/Dedalus_Projects/2D_boundary_forced_waves_v2/
-scp -r ef_snapshots/ mschee@ngws19.atmosp.physics.utoronto.ca:~/Documents/Dedalus_Projects/2D_boundary_forced_waves_v2/
+# check to see if arguments were passed
+if [ -z "$NAME" ]
+then
+	echo "No name specified, aborting script"
+	exit 1
+fi
+
+EXP_PATH=_experiments/${NAME}/
+
+LOCAL_EXP=~/Documents/Dedalus_Projects/2D_boundary_forced_waves_v2/_experiments/
+
+scp -r $EXP_PATH mschee@ngws19.atmosp.physics.utoronto.ca:${LOCAL_EXP}
+#scp -r ef_snapshots/ mschee@ngws19.atmosp.physics.utoronto.ca:~/Documents/Dedalus_Projects/2D_boundary_forced_waves_v2/
