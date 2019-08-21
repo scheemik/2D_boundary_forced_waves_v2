9fcf93e36c34419b5070a118b0bba23ad5e4bf5d
Mikhail <mschee@physics.utoronto.ca>
2019-08-21 12:32:38 -0400

    Now deletes non-merged snapshots before transfer to save space

--- push_from_Niagara.sh
+++ push_from_Niagara.sh
@@ -18,9 +18,12 @@ then
 	exit 1
 fi
 
-EXP_PATH=_experiments/${NAME}/
+EXP_PATH=./_experiments/${NAME}/
 
-LOCAL_EXP=~/Documents/Dedalus_Projects/2D_boundary_forced_waves_v2/_experiments/
+# Remove snapshot folders (not merged files) to save on transfer time
+find ${EXP_PATH}snapshots -maxdepth 1 -mindepth 1 -type d -exec rm -rf "{}" \;
 
-scp -r $EXP_PATH mschee@ngws19.atmosp.physics.utoronto.ca:${LOCAL_EXP}
+LOCAL_EXP=/Documents/Dedalus_Projects/2D_boundary_forced_waves_v2/_experiments/
+
+scp -r $EXP_PATH mschee@ngws19.atmosp.physics.utoronto.ca:~${LOCAL_EXP}
 #scp -r ef_snapshots/ mschee@ngws19.atmosp.physics.utoronto.ca:~/Documents/Dedalus_Projects/2D_boundary_forced_waves_v2/
